bc7d0a8032a854cc0680b22799f901b021118e71
Alyssa Arias <alyssaarias27@gmail.com>
2023-08-09 21:53:40 +0800

    added suitability test completed page

--- app/src/main/java/com/example/happytailapp/SuitabilityTest.java
+++ app/src/main/java/com/example/happytailapp/SuitabilityTest.java
@@ -67,7 +67,6 @@ public class SuitabilityTest extends AppCompatActivity {
                         finish();
                     }
                 }
-
             }
         });
 
--- app/src/main/java/com/example/happytailapp/TestComplete.java
+++ app/src/main/java/com/example/happytailapp/TestComplete.java
@@ -2,13 +2,38 @@ package com.example.happytailapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestComplete extends AppCompatActivity {
+    Button recoBtn, homeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_complete);
+
+        recoBtn = findViewById(R.id.recommendedButton);
+        homeBtn = findViewById(R.id.homepageButton);
+
+        recoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestComplete.this, RecommendedPets.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestComplete.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
